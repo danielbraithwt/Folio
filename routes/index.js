a507f4b6132169fb7edc871227b36e831426cd38
Daniel Braithwaite <danielbraithwt@gmail.com>
2015-01-06 10:40:11 +1300

    Changed The Routes File I added the schema to the routs file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var ProjectSchema = require("../schemas/project");
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+	// TODO: should load all the projects from the database
+
+  	res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
