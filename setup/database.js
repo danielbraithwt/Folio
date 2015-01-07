cf0b651decf085e25fc219766ab330184217c3ca
Daniel Braithwaite <danielbraithwt@gmail.com>
2015-01-07 15:26:17 +1300

    Database Setup File I created a file to set up the database tables and add the default user

---
+++ setup/database.js
@@ -0,0 +1,26 @@
+var mysql = require('mysql');
+var bcrypt = require('bcryptjs');
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	database: 'folio',
+	password: 'password'
+});
+
+
+console.log("[*] Database connection open");
+connection.connect();
+
+console.log("[*] Creating projects database");
+connection.query("CREATE TABLE IF NOT EXISTS projects(id int AUTO_INCREMENT, name varchar(200), description varchar(2000), languages varchar(3000), url varchar(500), giturl varchar(500), PRIMARY KEY(id));");
+
+console.log("[*] Creating users table");
+connection.query("CREATE TABLE IF NOT EXISTS users(email varchar(200), passwordhash varchar(2000));");
+
+console.log("[*] Creating default user, test@test.com (username), password (password)");
+var passwordhash = bcrypt.hashSync("password", 8);
+console.log("\t[>] Password hash: " + passwordhash);
+connection.query("INSERT INTO users(email, passwordhash) values('test@test.com', '" + passwordhash + "');");
+
+connection.end();
+console.log("[*] Configuration finished... Database connection closed");
