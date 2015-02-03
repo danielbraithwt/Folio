8d4f09829f98abc867785949e2b7f49fbe324b9c
Daniel Braithwaite <danielbraithwt@gmail.com>
2015-02-03 14:50:17 +1300

    MySQL Config Bug Fix

--- mysql/mysql-config.js
+++ mysql/mysql-config.js
@@ -2,7 +2,7 @@
 // variables and put them into an object
 module.exports = {
 	host: process.env.FOLIO_HOST || process.env.DB_HOST,
-	database: process.env.FOLIO_DATABASE || process.env.DB_NAME
-	user: process.env.FOLIO_USER,
+	database: process.env.FOLIO_DATABASE || process.env.DB_NAME,
+	user: process.env.FOLIO_USER || process.env.DB_USER,
 	password: process.env.FOLIO_PASSWORD || process.env.DB_PASS
 }
